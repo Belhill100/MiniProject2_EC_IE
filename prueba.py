f5f486e34a40df767684c43c33eb5d6774aaead9
IsaacEscobar <isak.escobarr@gmail.com>
2024-09-04 13:52:06 -0500

    Funcion para reproducir el audio con los efectos

--- prueba.py
+++ prueba.py
@@ -8,8 +8,21 @@ AUDIO_PATH = "audio\\"
 def contains_dollar(line):
     return any(char == "$" for char in line)
 
-audio_name = ""
+def play_audio(audio_name, audio_fx):
+	audio = oalOpen(AUDIO_PATH + audio_name)
+	audio.play()
+	match audio_fx:
+		case "derecha":
+			audio.set_position((1,0,0))
+		case "izquierda":
+			audio.set_position((-1,0,0))
+		case "eco":
+			audio.set_position((0,0,0))
+	time.sleep(1)
+	oalQuit()
+
 user_option = ''
+audio_name = ""
 audio_fx = ""
 with open('historia.txt', 'r', encoding='utf-8') as file:
 	for line in file:
@@ -20,10 +33,7 @@ with open('historia.txt', 'r', encoding='utf-8') as file:
 				else:
 					audio_name = file.readline().strip()
 					audio_fx = file.readline().strip()
-					audio = oalOpen(AUDIO_PATH + audio_name)
-					audio.play()
-					time.sleep(1)
-					oalQuit()
+					play_audio(audio_name, audio_fx)
 					for line in file:
 						if not contains_dollar(line):
 							print(line, end='')
