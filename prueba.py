4fec0faddee02e3485bd4ee9786889954adc0ed6
IsaacEscobar <isak.escobarr@gmail.com>
2024-09-03 21:45:28 -0500

    Lectura de la historia

--- prueba.py
+++ prueba.py
@@ -1,12 +1,34 @@
 from openal import * 
 import time
 
+story_point = '1'
+
 AUDIO_PATH = "audio\\"
 
-audio = oalOpen(AUDIO_PATH + "tv_noise.wav")
-audio.play()
+def contains_dollar(line):
+    return any(char == "$" for char in line)
 
-while audio.get_state() == AL_PLAYING:
-	time.sleep(1)
-
-oalQuit()
+audio_name = ""
+user_option = ''
+audio_fx = ""
+with open('historia.txt', 'r', encoding='utf-8') as file:
+	for line in file:
+		if line.startswith(story_point):
+			for line in file:
+				if not contains_dollar(line):
+					print(line, end='')
+				else:
+					audio_name = file.readline().strip()
+					audio_fx = file.readline().strip()
+					audio = oalOpen(AUDIO_PATH + audio_name)
+					audio.play()
+					time.sleep(1)
+					oalQuit()
+					for line in file:
+						if not contains_dollar(line):
+							print(line, end='')
+						else:
+							user_option = input()
+							story_point += '.' + str(user_option)
+							break
+					break
